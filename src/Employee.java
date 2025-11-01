9ce4c57842352f291ffaf67ece7b6a682973be86
Du11iNo <julian.rumi24@umt.edu.al>
2025-11-01 15:35:05 +0100

    Employee Super Class, still lacking some functions.

---
+++ src/Employee.java
@@ -0,0 +1,38 @@
+
+import java.time.*;
+public class Employee {
+    protected String NID;
+    protected String NameSurname;
+    protected LocalDate DateOfStart;
+    protected LocalDate Birthdate;
+    protected double Paycheck = 0.0;
+
+    public String getNID() {
+        return NID;
+    }
+
+    public void setNID(String NID) {
+        this.NID = NID;
+    }
+
+    public String getNameSurname() {
+        return NameSurname;
+    }
+
+    public void setNameSurname(String nameSurname) {
+        NameSurname = nameSurname;
+    }
+
+    public LocalDate getDateOfStart() {
+        return DateOfStart;
+    }
+
+    public LocalDate getBirthdate() {
+        return Birthdate;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        Birthdate = birthdate;
+    }
+
+}
