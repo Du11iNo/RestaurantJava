1a922a3df8ea55c1d2aec5dafcc03cd76605a85f
Du11iNo <julian.rumi24@umt.edu.al>
2025-11-01 16:02:35 +0100

    Restaurant class, holds main functions and lists; still lacking some functions.

---
+++ src/Restaurant.java
@@ -0,0 +1,60 @@
+
+import java.util.ArrayList;
+import java.time.*;
+public class Restaurant {
+
+    private ArrayList<Employee> EmployeeList = new ArrayList<>();
+    private ArrayList<Table> TableList = new ArrayList<>();
+
+    public boolean fireEmployee(String nid){
+        for (Employee employee: EmployeeList){
+            if (employee.getNID() == nid){
+                EmployeeList.remove(employee);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean addWaiter(String NID, String NameSurname, LocalDate Birthdate){
+        if (Birthdate.isAfter(LocalDate.now()))
+            return false;
+
+        EmployeeList.add(new Waiter(NID, NameSurname, Birthdate));
+        return true;
+    }
+
+    public boolean addChef(String NID, String NameSurname, LocalDate Birthdate){
+        if (Birthdate.isAfter(LocalDate.now()))
+            return false;
+
+        EmployeeList.add(new Chef(NID, NameSurname, Birthdate));
+        return true;
+    }
+
+    public void printEmployeeList(){
+        for (Employee emp: EmployeeList)
+            System.out.println(emp);
+    }
+
+    public void printTables(){
+        for (Table tab: TableList)
+            System.out.println(tab);
+    }
+
+    public void removeAllReservations(){
+        for (Table tab: TableList)
+            tab.setReserved(false);
+    }
+
+    public boolean setReservation(int ID, LocalDate date){
+        for (Table tab: TableList)
+            if (tab.getID() == ID){
+                tab.setReserved(true);
+                tab.reserveTable(date);
+                return true;
+            }
+        return false;
+    }
+
+}
