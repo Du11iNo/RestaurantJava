6f4cfb8a5858405c6a9e464dc8ca419ab6832867
Du11iNo <julian.rumi24@umt.edu.al>
2025-11-02 21:05:08 +0100

    Food to be part of Menu's (Order Array), most functions are done; Few fixes and protections needed.

---
+++ src/Order.java
@@ -0,0 +1,25 @@
+
+
+public class Order {
+    private final String Name;
+    private final double Price;
+
+    public String getName() {
+        return Name;
+    }
+
+    public double getPrice() {
+        return Price;
+    }
+
+    public Order(String name, double price) {
+        Name = name;
+        Price = price;
+    }
+
+    @Override
+    public String toString(){
+        return Name + ": $" + Price;
+    }
+
+}
