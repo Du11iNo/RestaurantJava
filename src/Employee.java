4ec413ca37f0d0499cc8bc1677671cedcf1fce78
AlastBombast <florian.rumi24@umt.edu.al>
2025-12-14 19:10:50 +0100

    Changed Employee to abstract, added constructor.

--- src/Employee.java
+++ src/Employee.java
@@ -2,20 +2,25 @@
 import java.time.*;
 
 //Super Class; used as inheritance
-public class Employee {
+public abstract class Employee {
     protected String NID;
     protected String NameSurname;
     protected LocalDate DateOfStart;
     protected LocalDate Birthdate;
     protected double Paycheck = 0.0;
 
+    public Employee(String NID, String nameSurname, LocalDate birthdate) {
+        this.NID = NID;
+        NameSurname = nameSurname;
+        DateOfStart = LocalDate.now();
+        Birthdate = birthdate;
+    }
+
     public String getNID() {
         return NID;
     }
 
-    public void setNID(String NID) {
-        this.NID = NID;
-    }
+    public void setNID(String NID) {this.NID = NID;}
 
     public String getNameSurname() {
         return NameSurname;
